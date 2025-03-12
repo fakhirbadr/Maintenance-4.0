53254ebec43bac8e78ca9e862f91e8313b04d485
fakhirbadr <125561626+fakhirbadr@users.noreply.github.com>
2025-03-12 13:37:29 +0000

    Update SidBar.jsx

--- src/components/SidBar.jsx
+++ src/components/SidBar.jsx
@@ -263,7 +263,7 @@ const Array1 = [
       </IconWrapper>
     ),
     path: "/SuiviDemande",
-    roleRequired: ["admin", "docteurs", "user"],
+    roleRequired: ["admin", "docteurs", "user","chargé de stock],
   },
   {
     text: "Utilisateur",
